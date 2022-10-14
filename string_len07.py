12ffe13e6c2d7649d83467fd58053bb9198c8a1c
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-14 11:21:11 +0500

    07

--- string_len07.py
+++ string_len07.py
@@ -12,37 +12,11 @@ def main(s1,s2,s3):
     b=len(s2)
     c=len(s3)
     ans=[]  
-    if a%2==1:
-        if b%2==1:
-            if c%2==1:
-                answer=[s1,s2,s3]
-    if a%2==1:
-        if b%2==1:
-            if c%2==0:
-                answer=[s1,s2]
-    if a%2==1:
-        if b%2==0:
-            if c%2==1:
-                answer=[s1,s3]
-    if a%2==0:
-        if b%2==1:
-            if c%2==1:
-                answer=[s2,s3]
-    if a%2==1:
-        if b%2==0:
-            if c%2==0:
-                answer=[s1]   
-    if a%2==0:
-        if b%2==1:
-            if c%2==0:
-                answer=[s2]
-    if a%2==0:
-        if b%2==0:
-            if c%2==1:
-                answer=[s3] 
-    if a%2==0:
-        if b%2==0:
-            if c%2==0:
-                answer=[]                                           
-    return str(answer)                                                                        
-print(main('example ','python','coder '))        
+    if a%2>0:
+        ans+=[s1]
+    if b%2>0:
+        ans+=[s2]
+    if c%2>0:
+        ans+=[s3]        
+    return str(ans)                                                                       
+print(main('example ','pytho n','cod er'))        
