6cd811b55e006a14e0f30b85f7d4ab9830dc0c8c
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-14 09:24:40 +0500

    07

--- string_len07.py
+++ string_len07.py
@@ -1,3 +1,6 @@
+from turtle import clear
+
+
 def main(s1,s2,s3):
     """
     Given three strings, s1, s2 and s3. return their odd lengths, example "[s1, s2]". If there is no odd length, return "[]".
@@ -45,4 +48,4 @@ def main(s1,s2,s3):
             if c%2==0:
                 answer=[]                                           
     return answer                                                                        
-print(main('example ','python','coder'))        
+print(main('example','python','coder'))        
