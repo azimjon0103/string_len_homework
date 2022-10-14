12b874ac454cae0f50c8f1192850388bd2a3d945
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-14 15:25:24 +0500

    07

--- string_len07.py
+++ string_len07.py
@@ -11,12 +11,12 @@ def main(s1,s2,s3):
     a=len(s1)
     b=len(s2)
     c=len(s3)
-    ans=[]  
-    if a%2>0:
-        ans+=[s1]
-    if b%2>0:
-        ans+=[s2]
-    if c%2>0:
-        ans+=[s3]        
-    return str(ans)                                                                       
+    ans='' 
+    if a%2==1:
+        ans+=s1+","
+    if b%2==1:
+        ans+=s2+","
+    if c%2==1:
+        ans+=s3+","        
+    return '['+ans+']'                                                                 
 print(main('example ','pytho n','cod er'))        
